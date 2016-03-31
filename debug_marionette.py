0c070d4f87180fe49c36e9915096274114a20e50
Allison Wilbur <allison@saucelabs.com>
2016-03-31 12:05:53 -0700

    add debug_marionette.py

---
+++ debug_marionette.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+firefox_capabilities = DesiredCapabilities.FIREFOX
+firefox_capabilities['marionette'] = True
+firefox_capabilities['marionette.logging'] = True
+firefox_capabilities['binary'] = 'D:\\Program Files\\Firefox 45\\firefox.exe'
+
+driver = webdriver.Remote(
+    desired_capabilities=firefox_capabilities,
+    command_executor="http://localhost:4443"
+)
+# driver = webdriver.Firefox(capabilities=firefox_capabilities, firefox_binary=firefox_capabilities['binary'],
+#                             executable_path='D:\\selenium\\wires-0.6.2-win.exe')
+print driver.desired_capabilities
+driver.get('http://theonion.com')
+
+driver.quit()
